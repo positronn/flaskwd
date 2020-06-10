e8eb9525f0478cd55518eb38f1e31a88225de3c9
mar <mar.dls@tutamail.com>
2020-06-09 23:27:19 -0500

    Database Mapping and session interaction.

--- hello.py
+++ hello.py
@@ -1,17 +1,23 @@
+import os
 from flask import Flask
 from flask import session
 from flask import render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
 from datetime import datetime
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -19,6 +25,27 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [DataRequired()])
     submit = SubmitField('Submit')
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+
+    def __repr__(self):
+        return fr'<Role {self.name}>'
+    
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+
+    def __repr__(self):
+        return fr'<User {self.username}>'
+    
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -32,15 +59,27 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     return render_template(
-        'index.html', form = form, name = session.get('name')
+        'index.html',
+        form = form,
+        name = session.get('name'),
+        known = session.get('known', False)
     )
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name)
+    return render_template(
+        'user.html',
+        name = name
+    )
--- runapp.sh
+++ runapp.sh
@@ -1,3 +1,2 @@
 export FLASK_APP=hello.py
 export FLASK_DEBUG=1
-flask run
