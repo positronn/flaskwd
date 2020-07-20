030a06edf5ca3008ef9c58a83eea20e62a3dd054
mar <mar.dls@tutamail.com>
2020-07-19 23:49:46 -0500

    Login support.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 # app/models.py
 from . import db
+from . import login_manager
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
+from flask_login import UserMixin
 
 
 class Role(db.Model):
@@ -15,19 +17,17 @@ class Role(db.Model):
     users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique = True, index = True)
     username = db.Column(db.String(64), unique = True, index = True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return fr'<User {self.username}>'
     
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    # user password
-    password_hash = db.Column(db.String(128))
-
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -38,3 +38,8 @@ class User(db.Model):
     
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
