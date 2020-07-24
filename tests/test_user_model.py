d2939ff43e9ef439ba70ee9e975e82a70ab95db3
mar <mar.dls@tutamail.com>
2020-07-24 16:59:01 -0500

    Caching hash of user avatar in db.

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -161,14 +161,15 @@ class UserModelTestCase(unittest.TestCase):
     def test_gravatar(self):
         u = User(email='john@example.com', password='cat')
         with self.app.test_request_context('/'):
-            gravatar = u.gravatar(secure = False)
+            gravatar = u.gravatar()
             gravatar_256 = u.gravatar(size=256)
             gravatar_pg = u.gravatar(rating='pg')
             gravatar_retro = u.gravatar(default='retro')
         with self.app.test_request_context('/', base_url='https://example.com'):
             gravatar_ssl = u.gravatar()
-        self.assertTrue('http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6' in gravatar)
+        self.assertTrue('https://secure.gravatar.com/avatar/' +
+                        'd4c74594d841139328695756648b6bd6' in gravatar)
         self.assertTrue('s=256' in gravatar_256)
         self.assertTrue('r=pg' in gravatar_pg)
         self.assertTrue('d=retro' in gravatar_retro)
-        self.assertTrue('https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6' in gravatar_ssl)
+    
