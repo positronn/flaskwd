fcfaec6a424e8d28830401afe226fe89fcee71f5
mar <mar.dls@tutamail.com>
2020-07-30 22:52:53 -0500

    Adding show followed posts on home page feature.

--- app/models.py
+++ app/models.py
@@ -160,6 +160,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default = True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
+        self.follow(self)
+    
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -265,6 +267,14 @@ class User(UserMixin, db.Model):
         if user.id is None:
             return False
         return self.followers.filter_by(follower_id = user.id).first() is not None
+    
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     @property
     def followed_posts(self):
